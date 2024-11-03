bf7b40ef428f4ecd2e2ed96b9e32af3289bca0e5
Kent Giang <kentgi154@gmail.com>
2024-11-03 01:00:54 -0800

    part 2 of lab 5 done

--- client/src/components/Expense/ExpenseItem.tsx
+++ client/src/components/Expense/ExpenseItem.tsx
@@ -4,21 +4,21 @@ import { AppContext } from "../../context/AppContext";
 
 const ExpenseItem = (currentExpense: Expense) => {
   // Exercise: Consume the AppContext here
-  const {expenses, setExpenses} = useContext(AppContext);
+  const { expenses, setExpenses } = useContext(AppContext);
   const handleDeleteExpense = (currentExpense: Expense) => {
     // Exercise: Remove expense from expenses context array
-    const updatedExpenses = expenses.filter(
-        (e) => e.id !== currentExpense.id
-    );
+    const updatedExpenses = expenses.filter((e) => e.id !== currentExpense.id);
     setExpenses(updatedExpenses);
   };
 
   return (
     <li className="list-group-item d-flex justify-content-between align-items-center">
-      <div>{currentExpense.name}</div>
+      <div>{currentExpense.description}</div>
       <div>${currentExpense.cost}</div>
       <div>
-        <button aria-label={currentExpense.name} onClick={() => handleDeleteExpense(currentExpense)}>x</button>
+        <button aria-label={currentExpense.description} onClick={() => handleDeleteExpense(currentExpense)}>
+          x
+        </button>
       </div>
     </li>
   );
